74cd98a7733a9c2c4178908269a9f6fd0d0abea2
Joben <josephebenge@gmail.com>
2017-11-11 10:32:21 -0500

    Adding navigation event arguments

---
+++ SimpleWPF.Core/Navigation/Arguments/NavigationEventArgs.cs
@@ -0,0 +1,48 @@
+﻿using SimpleWPF.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWPF.Core.Navigation.Arguments
+{
+    public delegate void BeforeNavigationEventHandler(object sender, NavigationEventArgs e);
+    public delegate void AfterNavigationEventHandler(object sender, NavigationEventArgs e);
+    public delegate void BeforeClosingEventHandler(object sender, WindowEventArgs e);
+    public delegate void AfterClosingEventHandler(object sender, WindowEventArgs e);
+
+    public class NavigationEventArgs : EventArgs
+    {
+        public SimpleViewModel ViewModelTo { get; private set; }
+        public SimpleViewModel ViewModelFrom { get; private set; }
+
+        public NavigationEventArgs()
+        {
+
+        }
+
+        public NavigationEventArgs(SimpleViewModel navigatingTo, SimpleViewModel navigatingFrom = null)
+        {
+            ViewModelTo = navigatingTo;
+            ViewModelFrom = navigatingFrom;
+        }
+    }
+
+    public class WindowEventArgs : EventArgs
+    {
+        public ISimpleWindow WindowTo { get; private set; }
+        public ISimpleWindow WindowFrom { get; private set; }
+
+        public WindowEventArgs()
+        {
+
+        }
+
+        public WindowEventArgs(ISimpleWindow windowTo, ISimpleWindow windowFrom = null)
+        {
+            WindowTo = windowTo;
+            WindowFrom = windowFrom;
+        }
+    }
+}
--- SimpleWPF.Core/Navigation/SimpleNavigationService.cs
+++ SimpleWPF.Core/Navigation/SimpleNavigationService.cs
@@ -1,4 +1,5 @@
 ﻿using SimpleWPF.Core.Core;
+using SimpleWPF.Core.Navigation.Arguments;
 using SimpleWPF.Core.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -13,12 +14,18 @@ namespace SimpleWPF.Core.Navigation
     {
         public static readonly SimpleNavigationService Instance = new SimpleNavigationService();
 
+        public ObservableCollection<SimpleViewModel> NavigationHistory { get; private set; } = new ObservableCollection<SimpleViewModel>();
         public ISimpleNavigationProvider Provider { get; private set; }
         public SimpleViewModel DefaultNavigation { get; private set; }
-
         public int MaxHistoryObjects { get; set; } = 5;
 
-        public ObservableCollection<SimpleViewModel> NavigationHistory { get; private set; } = new ObservableCollection<SimpleViewModel>();
+        private event BeforeNavigationEventHandler BeforeNavigation;
+        private event AfterNavigationEventHandler AfterNavigation;
+        private event BeforeClosingEventHandler BeforeClosing;
+        private event AfterClosingEventHandler AfterClosing;
+
+
+
 
         private SimpleNavigationService()
         {
@@ -27,6 +34,10 @@ namespace SimpleWPF.Core.Navigation
 
         public void Navigate(SimpleViewModel navObject)
         {
+            var args = new NavigationEventArgs(navObject, Provider.Current);
+
+            OnBeforeNavigate(this, args);
+
             if (Provider.Current != null && Provider.Current != navObject)
             {
                 if (NavigationHistory.Count >= MaxHistoryObjects)
@@ -38,12 +49,19 @@ namespace SimpleWPF.Core.Navigation
             }
 
             Provider.Current = navObject;
+
+            OnAfterNavigate(this, args);
         }
 
         public void NavigateWithNewWindow(SimpleViewModel navObject, ISimpleWindow newWindow)
         {
+            var args = new WindowEventArgs(newWindow, Provider.Window);
+
             Navigate(navObject);
+
+            OnBeforeClosing(this, args);
             Provider.Window.TransitionWindow(newWindow);
+            OnAfterClosing(this, args);
         }
 
         public void RegisterProvider(ISimpleNavigationProvider provider)
@@ -60,6 +78,26 @@ namespace SimpleWPF.Core.Navigation
                 if (Provider != null)
                     Provider.Current = navigationObject;
             }
+        }
+
+        public void OnBeforeNavigate(object sender, NavigationEventArgs e)
+        {
+            BeforeNavigation?.Invoke(this, e);
+        }
+
+        public void OnAfterNavigate(object sender, NavigationEventArgs e)
+        {
+            AfterNavigation?.Invoke(this, e);
+        }
+
+        public void OnBeforeClosing(object sender, WindowEventArgs e)
+        {
+            BeforeClosing?.Invoke(this, e);
+        }
+
+        public void OnAfterClosing(object sender, WindowEventArgs e)
+        {
+            AfterClosing?.Invoke(this, e);
         }
     }
 }
