cbe2a7c312bc9badf1ff42508bca0bf208081996
Joseph <josephebenge@gmail.com>
2018-05-20 14:02:06 -0400

    Added GoBack function

--- Samples/SingleWindowSampleApplication/ViewModels/RedViewModel.cs
+++ Samples/SingleWindowSampleApplication/ViewModels/RedViewModel.cs
@@ -10,23 +10,30 @@ namespace SingleWindowSampleApplication.ViewModels
 {
     public class RedViewModel : SimpleViewModel
     {
+        public ICommand GotoBackCommand { get; set; }
         public ICommand GotoBlueCommand { get; set; }
         public ICommand GotoYellowCommand { get; set; }
 
         public RedViewModel()
         {
+            GotoBackCommand = new SimpleRelayCommand(GotoBack);
             GotoBlueCommand = new SimpleRelayCommand(GotoBlue);
             GotoYellowCommand = new SimpleRelayCommand(GotoYellow);
+        }
+
+        private void GotoBack()
+        {
+            NavigateBack();
+        }
+
+        private void GotoBlue()
+        {
+            Navigate(new BlueViewModel());
         }
 
         private void GotoYellow()
         {
             Navigate(new YellowViewModel());
-        }
-
-        private void GotoBlue()
-        {
-            NavigateBack();
         }
     }
 }
