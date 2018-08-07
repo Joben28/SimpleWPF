91805a71094dcd950380e87e91755104eb330be2
Joseph <josephebenge@gmail.com>
2018-08-07 13:31:17 -0400

    Moved observer abstractions to new directory "Abstractions/Observer"

---
+++ SimpleWPF/Abstractions/Observer/IObservable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWPF
+{
+    internal interface IObservable
+    {
+        IList<IObserver> Observers { get; }
+        void Notify();
+        void Register(IObserver observer);
+        void Unregister(IObserver observer);
+    }
+}
