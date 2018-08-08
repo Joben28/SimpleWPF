a91a1c4bb4cca3eb7c18975e12c8f5ad95b873d1
Joseph <josephebenge@gmail.com>
2018-08-08 13:17:24 -0400

    Implemented a sample application for using the dialog service.

---
+++ DialogSampleApplication/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using SimpleWPF.Core;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DialogSampleApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            DataTemplateManager manager = new DataTemplateManager();
+            manager.LoadDataTemplatesByConvention();
+        }
+    }
+}
---
+++ DialogSampleApplication/Dialogs/Alert/AlertDialogViewModel.cs
@@ -0,0 +1,28 @@
+﻿using SimpleWPF.Dialog;
+using SimpleWPF.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DialogSampleApplication.Dialogs.Alert
+{
+    //This only returnes a simple DialogResults enum. However, the return type could be anything (string, object, etc.)
+    public class AlertDialogViewModel : DialogViewModelBase<DialogResults>
+    {
+        public ICommand OKCommand { get; private set; }
+
+        public AlertDialogViewModel(string title, string message) : base(title, message)
+        {
+            OKCommand = new RelayCommand<IDialogWindow>(OK);
+        }
+
+        //Window must be passed in param to close.
+        private void OK(IDialogWindow window)
+        {
+            CloseDialogWithResult(window, DialogResults.Undefined);
+        }
+    }
+}
